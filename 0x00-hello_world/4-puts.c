f08849daf42abd6151d269f8fa8040cbb898866e
Templeokeke <okeketemple02020@gmail.com>
2023-02-16 18:09:31 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdi
+/**
+ * - main - A program that prints a line with puts function
+ *   Return: 0 (Success)
+ *   */
+int main (void)
+{
+	puts ("/"Programming is like building a multilingual puzzle");
+	return (0);
+	)
